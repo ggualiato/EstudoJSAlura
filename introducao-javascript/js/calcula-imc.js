7372ba12359194f408c2a1cc53737c5834d453f9
Giovanni Gualiato <re035129@resource.com.br>
2017-08-08 17:23:05 -0300

    Aula 06 finalizada

---
+++ introducao-javascript/js/calcula-imc.js
@@ -0,0 +1,69 @@
+//selecionando pacientes
+var pacientes = document.querySelectorAll(".paciente");
+
+//repetição para cáculo do IMC de todos pacientes
+for (var i = 0; i < pacientes.length; i++){
+
+  var paciente = pacientes[i];
+
+  var tdPeso = paciente.querySelector(".info-peso");
+  var peso = tdPeso.textContent;
+
+  var tdAltura = paciente.querySelector(".info-altura");
+  var altura = tdAltura.textContent;
+
+  var tdImc = paciente.querySelector(".info-imc");
+
+  var pesoEhValido = validaPeso(peso);
+  var alturaEhValida = validaAltura(altura);
+
+  if (!pesoEhValido) {
+    console.log("Peso inválido!");
+
+    pesoEhValido = false;
+
+    tdImc.textContent = "Peso inválido!";
+
+    paciente.classList.add("paciente-invalido");
+  }
+  if (!alturaEhValida) {
+    console.log("Altura inválida!");
+
+    alturaEhValida = false;
+
+    tdImc.textContent = "Altura inválida!";
+
+    paciente.classList.add("paciente-invalido");
+  }
+
+  if (pesoEhValido && alturaEhValida) {
+    var imc = calculaImc(peso, altura);
+
+    tdImc.textContent = imc;
+  }
+}
+
+function calculaImc(peso, altura) {
+    var imc = 0;
+    imc = peso / (altura * altura);
+
+    return imc.toFixed(2);
+}
+
+//validações
+function validaPeso(peso) {
+    if (peso >= 0 && peso <= 1000) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function validaAltura(altura) {
+
+    if (altura >= 0 && altura <= 3.00) {
+        return true;
+    } else {
+        return false;
+    }
+}
